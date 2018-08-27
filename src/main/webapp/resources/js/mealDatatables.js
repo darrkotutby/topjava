f8b54f63433810e864f2b6138974f0d31bc4381f
Aliaksandr Pilipenka <ext.aliaksandr.pilipenka@tieto.com>
2018-08-27 17:55:09 +0300

    HW8 base

---
+++ src/main/webapp/resources/js/mealDatatables.js
@@ -0,0 +1,37 @@
+var ajaxUrl = "ajax/meals/";
+var datatableApi;
+
+// $(document).ready(function () {
+$(function () {
+    datatableApi = $("#datatable").DataTable({
+        "paging": false,
+        "info": true,
+        "columns": [
+            {
+                "data": "dateTime"
+            },
+            {
+                "data": "description"
+            },
+            {
+                "data": "calories"
+            },
+            {
+                "defaultContent": "Edit",
+                "orderable": false
+            },
+            {
+                "defaultContent": "Delete",
+                "orderable": false
+            }
+        ],
+        // TODO check datetime sorting
+        "order": [
+            [
+                0,
+                "desc"
+            ]
+        ]
+    });
+    makeEditable();
+});
